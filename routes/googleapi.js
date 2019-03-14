4c2e549e4809c0b488ee73951f7d829e9bb4e3b6
Your Name <you@example.com>
2019-03-14 19:39:45 +0530

    googleplus connection

--- routes/googleapi.js
+++ routes/googleapi.js
@@ -3,13 +3,17 @@ const express=require('express');
 const router=express.Router();
 const localstorage= require('local-storage');
 const config={
-  CLIENT_ID:"",
-  CLIENT_SECRET:"",
-  REDIRECT_URL:""}
+  CLIENT_ID:"161830841145-citdi0a9ne9pe14il7rsa8q9ka8q2d9p.apps.googleusercontent.com",
+  CLIENT_SECRET:"xLVfkxcY8SoRkhUxMuDVhD7R",
+  REDIRECT_URL:"http://localhost:3000/google/userinfo"}
 
 const oauth2Client = new google.auth.OAuth2(config.CLIENT_ID,config.CLIENT_SECRET,config.REDIRECT_URL);
 const scopes = [
+     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/contacts.readonly'];
 const url = oauth2Client.generateAuthUrl({
   access_type: 'offline',
@@ -34,10 +38,27 @@ router.get('/userData',(req,res,next)=>{
  	 const tokens=localstorage.get("access_token");
      oauth2Client.setCredentials(tokens);
      let userDetail=[];
+
+     const gmail = google.gmail({version: 'v1', auth:oauth2Client});
+      
+     gmail.users.getProfile({userId: 'me',personFields:'emailAddress'}, (err, res) => {
+       if (err) return console.log('The API returned an error: ' + err);
+      });
+      
+      const plus=google.plus({version: 'v1', auth:oauth2Client})
+      plus.people.get({userId: 'me',personFields:'displayName'}, (err, res) => {
+          if (err) return console.log('The API returned an error: ' + err);
+   
+          console.log(res);
+         }
+      );
        const service = google.people({version: 'v1', auth:oauth2Client});
+      
+
        service.people.connections.list({resourceName: 'people/me',personFields:'names,phoneNumbers'},(error,result)=>{
        if(error) return console.log(error);
        let userData=result.data.connections;
+     
        if(userData){
          	userData.forEach(people=>{
        		    if((people.names && people.names.length>0) && (people.phoneNumbers && people.phoneNumbers.length>0)){
